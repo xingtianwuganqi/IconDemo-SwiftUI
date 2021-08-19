252bc794760bd73210a8d46614a32d8c3e6d85de
Ethan <rxswift@126.com>
2021-08-19 19:21:01 +0800

    新建项目

---
+++ IconDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IconDemo
+//
+//  Created by Admin on 2021/8/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IconDemo/IconDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconDemoApp.swift
+//  IconDemo
+//
+//  Created by Admin on 2021/8/19.
+//
+
+import SwiftUI
+
+@main
+struct IconDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
